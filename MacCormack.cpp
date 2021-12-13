67e6822f0218a96e5b3064b7749ac2b6b3415a2e
taylorg <tegrubbs1@gmail.com>
2021-12-13 15:44:03 -0500

    should use abs for u_lid in timestep calc. using ru for moving wall rho

--- MacCormack.cpp
+++ MacCormack.cpp
@@ -3,7 +3,7 @@
 
 MacCormack::MacCormack() : Simulation() {
 
-  dt = 0.5 * dx / (1./mach + u_lid);
+  dt = 0.5 * dx / (1./mach + fabs(u_lid));
   cout << "MacCormack timestep defined by stability criteria: " << dt << endl;
 
   rs = create2dArray<double>(grid_size_x, grid_size_y);
@@ -125,9 +125,6 @@ void MacCormack::stationary_wall_corrector(size_t i, size_t j) {
 
 }
 
-// The nature of the upwind difference at the lid edges causes density at the corners to increase indefinitely.
-// I think the density at these corners should not differ too greatly from the neighboring gridpoints so
-// doing an average instead of the original difference makes more physical sense.
 void MacCormack::moving_wall_predictor(size_t i, size_t j) {
 
   // top moving lid
@@ -136,57 +133,14 @@ void MacCormack::moving_wall_predictor(size_t i, size_t j) {
     if (boundary[i-1][j] == 1 || boundary[i+1][j] == 1) {
       rs[i][j] = r[i][j] + 0.5*a2 * (-rv[i][j-2] + 4.*rv[i][j-1] - 3.*rv[i][j]);
     } else {
-      rs[i][j] = r[i][j] - 0.5*a1*u_lid * (r[i+1][j] - r[i-1][j]) + 0.5*a2 * (-rv[i][j-2] + 4.*rv[i][j-1] - 3.*rv[i][j]);
+      rs[i][j] = r[i][j] - 0.5*a1 * (ru[i+1][j] - ru[i-1][j]) + 0.5*a2 * (-rv[i][j-2] + 4.*rv[i][j-1] - 3.*rv[i][j]);
     }
 
     rus[i][j] = u_lid * rs[i][j];
     rvs[i][j] = 0.;
   }
 
-  // // bottom moving lid
-  // else if (boundary[i][j-1] == -1) {
-  //   // Check if this is a corner point
-  //   if (boundary[i-1][j] == -1) {
-  //     rs[i][j] = 1./3. * (r[i][j] + r[i+1][j] + r[i][j+1]);
-  //   } else if (boundary[i+1][j] == -1) {
-  //     rs[i][j] = 1./3. * (r[i][j] + r[i-1][j] + r[i][j+1]);
-  //   } else {
-  //     rs[i][j] = r[i][j] - 0.5*a1*u_lid * (r[i+1][j] - r[i-1][j]) - 0.5*a2 * (-rv[i][j+2] + 4.*rv[i][j+1] - 3.*rv[i][j]);
-  //   }
 
-  //   rus[i][j] = u_lid * rs[i][j];
-  //   rvs[i][j] = 0.;
-  // }
-
-  // // left moving lid
-  // else if (boundary[i-1][j] == -1) {
-  //   // Check if this is a corner point
-  //   if (boundary[i][j-1] == -1) {
-  //     rs[i][j] = 1./3. * (r[i][j] + r[i+1][j] + r[i][j+1]);
-  //   } else if (boundary[i][j+1] == -1) {
-  //     rs[i][j] = 1./3. * (r[i][j] + r[i+1][j] + r[i][j-1]);
-  //   } else {
-  //     rs[i][j] = r[i][j] - 0.5*a2*u_lid * (r[i][j+1] - r[i][j-1]) - 0.5*a1 * (-ru[i+2][j] + 4.*ru[i+1][j] - 3.*ru[i][j]);
-  //   }
-
-  //   rus[i][j] = 0.;
-  //   rvs[i][j] = u_lid * rs[i][j];
-  // }
-
-  // // right moving lid
-  // else if (boundary[i+1][j] == -1) {
-  //   // Check if this is a corner point
-  //   if (boundary[i][j-1] == -1) {
-  //     rs[i][j] = 1./3. * (r[i][j] + r[i-1][j] + r[i][j+1]);
-  //   } else if (boundary[i][j+1] == -1) {
-  //     rs[i][j] = 1./3. * (r[i][j] + r[i-1][j] + r[i][j-1]);
-  //   } else {
-  //     rs[i][j] = r[i][j] - 0.5*a2*u_lid * (r[i][j+1] - r[i][j-1]) + 0.5*a1 * (-ru[i-2][j] + 4.*ru[i-1][j] - 3.*ru[i][j]);
-  //   }
-
-  //   rus[i][j] = 0.;
-  //   rvs[i][j] = u_lid * rs[i][j];
-  // }
 
 }
 
@@ -197,51 +151,14 @@ void MacCormack::moving_wall_corrector(size_t i, size_t j) {
     if (boundary[i-1][j] == 1 || boundary[i+1][j] == 1) {
       r[i][j] = 0.5 * (r[i][j] + rs[i][j] + 0.5*a2 * (-rvs[i][j-2] + 4.*rvs[i][j-1] - 3.*rvs[i][j]));
     }  else {
-      r[i][j] = 0.5 * (r[i][j] + rs[i][j] - 0.5*a1*u_lid * (rs[i+1][j] - rs[i-1][j]) + 0.5*a2 * (-rvs[i][j-2] + 4.*rvs[i][j-1] - 3.*rvs[i][j]));
+      r[i][j] = 0.5 * (r[i][j] + rs[i][j] - 0.5*a1 * (rus[i+1][j] - rus[i-1][j]) + 0.5*a2 * (-rvs[i][j-2] + 4.*rvs[i][j-1] - 3.*rvs[i][j]));
 
     }
     ru[i][j] = r[i][j] * u_lid;
     rv[i][j] = 0.;
   }
 
-  // // bottom moving lid
-  // else if (boundary[i][j-1] == -1) {
-  //   if (boundary[i-1][j] == -1) {
-  //     r[i][j] = 1./3. * (rs[i][j] + rs[i+1][j] + rs[i][j+1]);
-  //   } else if (boundary[i+1][j] == -1) {
-  //     r[i][j] = 1./3. * (rs[i][j] + rs[i-1][j] + rs[i][j+1]);
-  //   } else {
-  //     r[i][j] = 0.5 * (r[i][j] + rs[i][j] - 0.5*a1*u_lid * (rs[i+1][j] - rs[i-1][j]) - 0.5*a2 * (-rvs[i][j+2] + 4.*rvs[i][j+1] - 3.*rvs[i][j]));
-  //     ru[i][j] = r[i][j] * u_lid;
-  //     rv[i][j] = 0.;
-  //   }
-  // }
 
-  // // left moving lid
-  // else if (boundary[i-1][j] == -1) {
-  //   if (boundary[i][j-1] == -1) {
-  //     r[i][j] = 1./3. * (rs[i][j] + rs[i+1][j] + rs[i][j+1]);
-  //   } else if (boundary[i][j+1] == -1) {
-  //     r[i][j] = 1./3. * (rs[i][j] + rs[i+1][j] + rs[i][j-1]);
-  //   } else {
-  //     r[i][j] = 0.5 * (r[i][j] + rs[i][j] - 0.5*a2*u_lid * (rs[i][j+1] - rs[i][j-1]) - 0.5*a1 * (-rus[i+2][j] + 4.*rus[i+1][j] - 3.*rus[i][j]));
-  //     ru[i][j] = 0.;
-  //     rv[i][j] = r[i][j] * u_lid;
-  //   }
-  // }
-
-  // // right moving lid
-  // else if (boundary[i+1][j] == -1) {
-  //   if (boundary[i][j-1] == -1) {
-  //     r[i][j] = 1./3. * (rs[i][j] + rs[i-1][j] + rs[i][j+1]);
-  //   } else if (boundary[i][j+1] == -1) {
-  //     r[i][j] = 1./3. * (rs[i][j] + rs[i-1][j] + rs[i][j-1]);
-  //   } else {
-  //     r[i][j] = 0.5 * (r[i][j] + rs[i][j] - 0.5*a2*u_lid * (rs[i][j+1] - rs[i][j-1]) + 0.5*a1 * (-rus[i-2][j] + 4.*rus[i-1][j] - 3.*rus[i][j]));
-  //     ru[i][j] = 0.;
-  //     rv[i][j] = r[i][j] * u_lid;
-  //   }
-  // }
 }
 
 void MacCormack::run_solver_step() {
